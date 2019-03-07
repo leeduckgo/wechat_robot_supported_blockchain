15a11e99c3b3550a59f7d4260fbe5c8ef0182282
leeduckgo <albertschr@163.com>
2019-03-07 21:30:26 +0800

    [main] update main module

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from random import choice
 from wxpy import *
 bot = Bot()
+group = bot.groups().search('超越微信机器人')[0]
+
+@bot.register(group)
+def reply_group(msg):
+    global group
+    print(msg)
+    if str.find(msg.text, "随机超越") != -1:
+        group.send_image("pics/ycy.png")
+
+embed() #阻塞线程不退出
