a2bf174840f3b4f90fcaf4d93c39ae9914605353
LXS <547498936@qq.com>
2019-03-10 00:59:08 +0800

    添加超越智能聊天

--- run.py
+++ run.py
@@ -1,24 +1,34 @@
 # -*- coding: utf-8 -*-
 from wxpy import embed,Bot,Tuling
 from common.group_replier import Replier # 导入即运行
+from common.group_manager import Manager
+from common.ycy_replier import YcyReplier
 import os
 from common.logger import Logger
-from settings import PROJECT_PATH, GTOUP1
+from settings import PROJECT_PATH, GROUP1
 
 # === init ===
 
 bot = Bot()
 bot.enable_puid()
-group = bot.groups().search(GTOUP1)[0]
-list_dir = os.listdir(os.path.join(PROJECT_PATH, 'pics'))
+group = bot.groups().search(GROUP1)[0]
+list_dir = os.listdir(os.path.join('resources', 'pics'))
 logger = Logger()
 tuling=Tuling(api_key='e889671fd22348528747941d7e563e02')
+manager = Manager(group)
 
+# === manager operation ===
+
+members = manager.all_members()
+logger.info(members)
+ycy_replier = YcyReplier()
+replier = Replier(bot, group, list_dir, logger, ycy_replier)
 # === main process ===
 @bot.register(group)
 def reply_group(msg):
-    """群组消息回复"""
-    replier = Replier(bot, group, list_dir, logger)
+    logger.info(msg)
+    """群组消息回复""" 
     replier.handle_msg(msg, tuling)
 
-embed()  # 阻塞线程不退出
+
+embed()  # 阻塞线程不退出'
--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 import os
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+RESOURCES_PATH = os.path.join(PROJECT_PATH, 'resources','text','words.csv')
 LOG_DIRECTORY = os.path.join(PROJECT_PATH, 'log')
 LOG_LEVEL = 'DEBUG'
-GTOUP1 = '超越微信机器人'
+GROUP1 = '超越微信机器人'
