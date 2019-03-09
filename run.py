45803674c8a27da09dfbacf07db934cd62b72dbf
LXS <547498936@qq.com>
2019-03-09 18:45:23 +0800

    解决冲突

--- run.py
+++ run.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
 from wxpy import embed,Bot,Tuling
 from common.group_replier import Replier # 导入即运行
-=======
-from wxpy import embed, Bot
-from common.group_replier import Replier
 from common.group_manager import Manager
->>>>>>> upstream/master
 import os
 from common.logger import Logger
 from settings import PROJECT_PATH, GROUP1
@@ -18,17 +13,13 @@ bot.enable_puid()
 group = bot.groups().search(GROUP1)[0]
 list_dir = os.listdir(os.path.join(PROJECT_PATH, 'pics'))
 logger = Logger()
-<<<<<<< HEAD
 tuling=Tuling(api_key='e889671fd22348528747941d7e563e02')
-=======
 manager = Manager(group)
 
 # === manager operation ===
 
 members = manager.all_members()
 logger.info(members)
-
->>>>>>> upstream/master
 
 # === main process ===
 @bot.register(group)
