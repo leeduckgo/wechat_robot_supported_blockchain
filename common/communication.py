7462d73934dcde093316529a15bbd4bebbab312d
王凡 <wangfan@botpy.com>
2019-03-15 19:27:58 +0800

    add message

--- common/communication.py
+++ common/communication.py
@@ -14,3 +14,21 @@ class Requester(object):
         url = self.node + path
         r = requests.get(url, params=params)
         return r.json()
+
+
+def create_messages(name='',  content='', fans_id='',):
+    """
+    创建留言
+    :param name:
+    :param content: 留言内容
+    :param fans_id:
+    :return:
+    """
+    url = 'http://ahasmarter.com/api/v1/ycy/messages/create'
+    data = {
+        'name': name,
+        'fans_id': fans_id,
+        'context': content,
+    }
+    r = requests.post(url, json=data)
+    return r.status_code
--- common/message_replier.py
+++ common/message_replier.py
@@ -4,10 +4,12 @@ from wxpy import Tuling
 from common.logger import Logger
 from common.ycy_replier import YcyReplier
 from settings import TULING_KEY
+import re
+from common.communication import create_messages
 
 
 class Replier(object):
-    """消息回复"""
+    """回复消息"""
 
     def __init__(self):
         # self.group = group
@@ -22,9 +24,25 @@ class Replier(object):
         self.log.info('choose:-->{}'.format(path))
         return os.path.join("resources", "pics", path)
 
+    def handle_leave_message(self, msg):
+        """处理留言"""
+        is_leave_message = re.search(r'(留言:|留言：)(.*)', msg.text)
+        if is_leave_message:
+            content = is_leave_message.group(2).strip()  # 获取第二组内容并去除前后空格
+            self.log.info('留言内容:{}'.format(content))
+            status = create_messages(name=msg.member.name, content=content, fans_id=msg.member.puid)
+            if status == 200:
+                return 'text', '@' + msg.member.name + ' ' + "留言成功！点击 {} 可查看你的留言".format(
+                    'http://ycy.ahasmarter.com/'
+                )
+            else:
+                return 'text', '@' + msg.member.name + ' ' + "留言失败！稍后再尝试吧"
+        return '', ''
+
     def handle_msg(self, msg):
         """处理回复消息"""
         self.log.info(msg)
+
         if msg.text == "天降超越":
             path = self.random_img()
             self.log.debug(path)
@@ -33,13 +51,18 @@ class Replier(object):
         if str.find(msg.text, "燃烧") != -1:
             # self.group.send()
             return 'text', "燃烧我的卡路里！"
-        if not msg.is_at:  # 如果没有@到机器人，不进行回应
-            return '', ''
-        else:
+
+        if msg.is_at:  # 如果没有@到机器人，不进行回应
+            typ, content = self.handle_leave_message(msg)  # 处理留言请求
+            if typ:
+                self.log.info(content)
+                return typ, content
             real_msg = msg.text.split()
             self.log.debug("send： " + real_msg[len(real_msg) - 1])
             respond_msg = self.ycy.reply_text(real_msg[len(real_msg) - 1])
             if respond_msg:
-                return 'text', '@' + msg.member.display_name + ' ' + respond_msg
+                return 'text', '@' + msg.member.name + ' ' + respond_msg
             else:
                 return 'text', self.tuling.reply_text(msg).replace("图灵机器人", "超越宝宝")
+
+        return '', ''
