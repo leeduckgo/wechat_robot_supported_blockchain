12fa5e605270deb5416f4b13fc12f646ef8a6dcd
王凡 <wangfan@botpy.com>
2019-03-15 11:34:45 +0800

    降低代码耦合度,增加监听所有群聊

---
+++ common/message_replier.py
@@ -0,0 +1,40 @@
+from random import choice
+import os
+from wxpy import Tuling
+from common.logger import Logger
+from common.ycy_replier import YcyReplier
+
+class Replier(object):
+    def __init__(self):
+        # self.group = group
+        self.log = Logger()
+        self.ycy = YcyReplier()
+        self.tuling = Tuling(api_key='e889671fd22348528747941d7e563e02')
+
+    def random_img(self):
+        """随机获取图片"""
+        list_dir = os.listdir(os.path.join('resources', 'pics'))
+        path = choice(list_dir)
+        self.log.info('choose:-->{}'.format(path))
+        return os.path.join("resources", "pics", path)
+
+    def handle_msg(self, msg):
+        self.log.info(msg)
+        if msg.text == "天降超越":
+            path = self.random_img()
+            self.log.debug(path)
+            # self.group.send_image(path)
+            return 'img', path
+        if str.find(msg.text, "燃烧") != -1:
+            # self.group.send()
+            return 'text', "燃烧我的卡路里！"
+        if not msg.is_at:  # 如果没有@到机器人，不进行回应
+            return '', ''
+        else:
+            real_msg = msg.text.split()
+            self.log.debug("send： " + real_msg[len(real_msg) - 1])
+            respond_msg = self.ycy.reply_text(real_msg[len(real_msg) - 1])
+            if respond_msg:
+                return 'text', '@' + msg.member.display_name + ' ' + respond_msg
+            else:
+                return 'text', self.tuling.reply_text(msg).replace("图灵机器人", "超越宝宝")
