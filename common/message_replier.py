992716420d0ebd63d78cc2b00a49e1a41fdfc151
王凡 <wangfan@botpy.com>
2019-03-18 18:34:52 +0800

    支持多群游戏

--- common/message_replier.py
+++ common/message_replier.py
@@ -24,10 +24,7 @@ class Replier(object):
         self.log = Logger()
         self.ycy = YcyReplier()
         self.tuling = Tuling(api_key=TULING_KEY)
-        self.rsp_game = RspGame(5)
-        self.rsp_game_player_name = ''
-        self.rsp_game_flag = False  # 是否开启石头剪刀布游戏
-        self.start_game_time = None  # 游戏开始时间
+        self.rsp_game_player_map = {}
 
     def random_img(self):
         """
@@ -67,7 +64,7 @@ class Replier(object):
         real_msg = msg.text.split()
         if real_msg[len(real_msg) - 1] != "本群简介":
             return empty_result
-        d = get_group_introduction(msg.sender.group.puid)
+        d = get_group_introduction(msg.member.group.puid)
         if not d:
             return empty_result
         return 'text', d.get('introduction'), ''
@@ -78,13 +75,26 @@ class Replier(object):
         :param msg:
         :return:
         """
+        group_id = msg.member.group.puid  # 群组唯一id
+        name = msg.member.display_name  # 玩家名
+        id = msg.member.puid  # 玩家id
         real_msg = msg.text.split()
         if real_msg[len(real_msg) - 1] == "石头剪刀布" or real_msg[len(real_msg) - 1] == "剪刀石头布" \
                 or real_msg[len(real_msg) - 1] == "猜拳":
-            self.rsp_game_player_name = msg.member.display_name
-            self.rsp_game.start(msg.member.display_name)
-            self.start_game_time = datetime.now()
-            self.rsp_game_flag = True
+            if group_id not in self.rsp_game_player_map.keys():  # 如果字典中不存在该群组id,构建字典
+                # { 群组id : {玩家id: [游戏对象 , 开始游戏的时间, 玩家名]}}
+                self.rsp_game_player_map.update(
+                    {
+                        group_id: {
+                            id: [RspGame(5), now_to_datetime4(), name]
+                        }
+                    }
+                )
+            elif group_id in self.rsp_game_player_map.keys() and \
+                    id not in self.rsp_game_player_map.get(group_id, {}).keys():  # 每个群只能同时运行一个游戏避免混乱
+                return 'text', '@' + msg.member.display_name + \
+                       " 有玩家在玩哟,稍等一下",''
+            self.rsp_game_player_map[group_id][id][0].start(name)  # 开始游戏
             return 'text', '@' + msg.member.display_name + \
                    " 石头剪刀布开始，你先出吧，赢了我有奖励哦(五局三胜)", ''
         return empty_result
@@ -95,24 +105,27 @@ class Replier(object):
         :param msg:
         :return:
         """
-        if self.start_game_time:
-            is_overtime = now_to_datetime4() > five_minutes_later(self.start_game_time)
+        group_id = msg.member.group.puid
+        name = msg.member.display_name
+        id = msg.member.puid
+        player_map = self.rsp_game_player_map
+        # 如果字典中包含群组id并且 玩家id在字典中
+        if player_map.get(group_id) and id in player_map.get(group_id, {}).keys():
+            is_overtime = now_to_datetime4() > five_minutes_later(player_map[id][1])
             if is_overtime:
-                self.rsp_game_flag = False
-                self.start_game_time = None
-                msg = '@' + self.rsp_game_player_name + ' 游戏已经超时自动终止了呀!'
+                msg = '@' + name + ' 游戏已经超时自动终止了呀!'
                 msg.chat.send_msg(msg)
-                self.rsp_game_player_name = ''
+                player_map.pop(group_id)  # 超时删除群组id对应的字典
 
-        if self.rsp_game_flag:
-            if self.rsp_game_player_name != msg.member.display_name:  # 不是玩家的消息，不进行回应
+        if player_map.get(group_id):
+            if id not in player_map.get(group_id, {}).keys():  # 不是玩家的消息，不进行回应
                 return 'text', '@' + msg.member.display_name + " 先等等哦，我正在跟@" + \
-                       self.rsp_game_player_name + " 玩石头剪刀布", ''
+                       player_map[id][2] + " 玩石头剪刀布", ''
             else:
-                cancel, result, pic = self.rsp_game.play(msg)
+                cancel, result, pic = player_map[id][0].play(msg)  # 玩游戏
                 self.log.debug('game result:{} pic:{}'.format(result, pic))
                 if cancel == 1:
-                    self.rsp_game_flag = False
+                    player_map.pop(group_id)  # 如果游戏结束, 删除群组id对应的字典
                 return 'both', pic, result
         typ, content1, content2 = self.finger_guessing_game(msg)  # 猜拳游戏
         if typ == 'text':
@@ -129,7 +142,6 @@ class Replier(object):
         if msg.text in ("杨超越", "天降超越"):  # todo 待增加
             path = self.random_img()
             self.log.debug(path)
-            # self.group.send_image(path)
             return 'img', path, ''
 
         real_msg = msg.text.split()
--- run.py
+++ run.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-import traceback
 from time import sleep
 from wxpy import embed
 from wxpy import Bot
 from wxpy import Group
-from wxpy import ensure_one
-from wxpy import get_wechat_logger
 from common.message_replier import Replier
 from common.logger import Logger
 
@@ -17,37 +14,31 @@ bot.enable_puid()
 log = Logger()  # 项目logger对象
 replier = Replier()
 
-
 # === main process ===
 if __name__ == '__main__':
-    try:
-        # 自动接受新的好友请求
-        @bot.register(msg_types='Friends')
-        def auto_accept_friends(msg):
-            """接受好友请求"""
-            new_friend = msg.card.accept()
-            # 向新的好友发送消息
-            new_friend.send('你好呀,我是全村的希望!')  # todo 内容待定 附加功能介绍
+    # 自动接受新的好友请求
+    @bot.register(msg_types='Friends')
+    def auto_accept_friends(msg):
+        """接受好友请求"""
+        new_friend = msg.card.accept()
+        # 向新的好友发送消息
+        new_friend.send('你好呀,我是全村的希望!')  # todo 内容待定 附加功能介绍
 
-        @bot.register()
-        def reply_message(msg):
-            """消息回复"""
-            if type(msg.sender) == Group:  # 所有群组消息
-                typ, content1, content2 = replier.handle_msg(msg)
-                if typ == 'text':
-                    msg.reply_msg(content1)
-                elif typ == 'img':
-                    msg.reply_image(content1)
-                elif typ == 'both':
-                    msg.reply_image(content1)
-                    sleep(1)
-                    msg.reply_msg(content2)
-            else:  # todo 私聊消息
-                log.info(msg.text)
-    except Exception:
-        # 找到需要接收日志的群 -- `ensure_one()` 用于确保找到的结果是唯一的，避免发错地方
-        group_receiver = ensure_one(bot.groups().search('超越微信机器人')) # todo 暂时
-        logger = get_wechat_logger(group_receiver)  # 重大错误监控,发送消息至指定群聊
-        logger.error(traceback.format_exc())
+    @bot.register()
+    def reply_message(msg):
+        """消息回复"""
+        if type(msg.sender) == Group:  # 所有群组消息
+            typ, content1, content2 = replier.handle_msg(msg)
+            if typ == 'text':
+                msg.reply_msg(content1)
+            elif typ == 'img':
+                msg.reply_image(content1)
+            elif typ == 'both':
+                msg.reply_image(content1)
+                sleep(1)
+                msg.reply_msg(content2)
+        else:  # todo 私聊消息
+            log.info(msg.text)
+
 
     embed()  # 阻塞线程不退出'
