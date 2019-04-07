9c922d31907553c208a65f800d127dff849910d2
leeduckgo <albertschr@163.com>
2019-04-07 13:44:49 +0800

    feature[redbag]: complete redbag func

--- run.py
+++ run.py
@@ -25,21 +25,21 @@ if __name__ == '__main__':
         logger.info(msg.sender)
         """消息回复"""
         if type(msg.sender) == Group:  # 所有群组消息
-            # if msg.sender.puid in ["f3619053"]:  # 限定群组
-            logger.info("=== start ===")
-            replier.set_group(msg.sender.puid) # 获取群信息
-            logger.info(msg.member.puid)
-            if msg.type == "Note":
-                replier.update_user_info(msg)
-            typ, content1, content2 = replier.handle_group_msg(msg)
-            if typ == 'text':
-                msg.reply_msg(content1)
-            elif typ == 'img':
-                msg.reply_image(content1)
-            elif typ == 'both':
-                msg.reply_image(content1)
-                sleep(1)
-                msg.reply_msg(content2)
+            if msg.sender.puid in ["1f423133", "4193b4db"]:  # 限定群组
+                logger.info("=== start ===")
+                replier.set_group(msg.sender.puid) # 获取群信息
+                logger.info(msg.member.puid)
+                if msg.type == "Note":
+                    replier.update_user_info(msg)
+                typ, content1, content2 = replier.handle_group_msg(msg)
+                if typ == 'text':
+                    msg.reply_msg(content1)
+                elif typ == 'img':
+                    msg.reply_image(content1)
+                elif typ == 'both':
+                    msg.reply_image(content1)
+                    sleep(1)
+                    msg.reply_msg(content2)
         # else:  # todo 私聊消息
         #     replier.handle_solo_msg(msg)
         #     logger.info(msg)
